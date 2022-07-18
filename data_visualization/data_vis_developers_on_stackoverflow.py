e8213ed76ce2ac4f6628109db816eadccab832f3
Lama Radwan <lama.radwan96@gmail.com>
2022-07-18 14:33:52 +0300

    added visualization for stackoverflow developers

---
+++ data_visualization/data_vis_developers_on_stackoverflow.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def developers_on_stackoverflow():
+    visitors = pd.read_csv('../csv/survey_results_public.csv')
+    employment = visitors['Employment'].value_counts()
+    type_of_emp = employment.index.tolist()
+    number = employment.values.tolist()
+    fig = plt.figure(figsize=(10, 8))
+    plt.pie(number, labels=type_of_emp, autopct='%1.2f%%')
+    plt.show()
+
+
+if __name__ == '__main__':
+    developers_on_stackoverflow()
+
