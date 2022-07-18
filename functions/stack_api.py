772f351ebac01c4b79e9f4dc87d9ded9b1a30454
unknown <mohlebzo1@gmail.com>
2022-07-18 11:18:12 +0300

    Feat: added a function that gets the highest voted questions under a specific tag

--- functions/stack_api.py
+++ functions/stack_api.py
@@ -12,5 +12,16 @@ def get_search_results(q_text, tag, number_of_results):
     return returned_results
 
 
+def get_most_voted_results(tag, number_of_results):
+    SITE = StackAPI('stackoverflow')
+    SITE.page_size = number_of_results
+    SITE.max_pages = 1
+    search_results = SITE.fetch('questions', sort="votes", tgged=tag)
+    returned_results = [{"Title": i["title"], "Link": i["link"], "votes": i["score"]} for i in search_results["items"]]
+    # print(json.dumps(returned_results, indent=4))
+    return returned_results
+
+
 if __name__ == "__main__":
-    print(get_search_results("How to split a list in python", "python", 30))
+    print(get_search_results("How to split a list in python", "python", 10))
+    print(get_most_voted_results("python", 5))
